a253acb2f51ebe72bffedcbe6dbc31696508597b
nebular-lab <hiranodaisuke0721@gmail.com>
2024-03-08 14:09:50 +0900

    selectedPrefTypeIndexステートの設定

---
+++ src/store/selectedPrefTypeIndex.ts
@@ -0,0 +1,15 @@
+import { create } from 'zustand';
+
+type Store = {
+  selectedPrefType: number;
+  setSelectedPrefType: (selectedPrefType: number) => void;
+};
+
+// 「総人口」:0,「年少人口」:1,「生産年齢人口」:2,「老年人口」:3
+// ラベルが増えても対応できるように番号で管理
+const useSelectedPrefTypeIndex = create<Store>((set) => ({
+  selectedPrefType: 0,
+  setSelectedPrefType: (selectedPrefType: number) => set({ selectedPrefType }),
+}));
+
+export default useSelectedPrefTypeIndex;
